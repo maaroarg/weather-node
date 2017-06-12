fe4833624119e74eddefe92b409fd3d61167065b
Mario Romero <mario.romero@avantrip.com>
2017-06-12 18:22:58 -0300

    first commit

---
+++ playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log('Starting app');
+
+setTimeout(()=>{
+    console.log('Inside 1st callback');
+},2000);
+
+
+setTimeout(()=>{
+    console.log('Inside 2nd callback');
+},0);
+
+console.log('Finishing up');
